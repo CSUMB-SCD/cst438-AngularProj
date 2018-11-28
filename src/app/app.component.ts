7723becadc6f66188334ab9e69117dc57ba5e7ac
Luis Valencia <lvalencia@csumb.edu>
2018-11-27 21:30:47 -0800

    Implemented nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit, ElementRef, AfterViewInit} from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'homework3';
+export class AppComponent implements OnInit {
+  title = 'Look jQuery Animation working in action!';
+
+public ngOnInit() {
+
+$(document).ready(function($) {
+  $('.menu-icon').on('click', function() {
+    $(this).toggleClass('open');
+    $('.container').toggleClass('nav-open');
+    $('nav ul li').toggleClass('animate');
+  });
+
+});
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProdListComponent } from './prod-list/prod-list.component';
+
+const appRoutes: Routes = [
+ { path: 'list', component: ProdListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
