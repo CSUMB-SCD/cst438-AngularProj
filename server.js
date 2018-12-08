271525caa7fa4a30e1ce966a06539f4c617e2f33
Adrian Martinez <adrianmartinez@MacBookAir.local>
2018-12-07 21:20:11 -0800

    host fix2

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
 //Install express server
-
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
-
-
 // Serve only the static files form the dist directory
-
-app.use(express.static(__dirname + '/dist/cst438-AngularProj'));
-
-
+app.use(express.static('./dist/cst438-AngularProj'));
 
 app.get('/*', function(req,res) {
-
-
-
-res.sendFile(path.join(__dirname+'/dist/cst438-AngularProj/index.html'));
-
+  res.sendFile(path.join(__dirname,'/dist/cst438-AngularProj/index.html'));
 });
-
-
 
 // Start the app by listening on the default Heroku port
-
 app.listen(process.env.PORT || 8080);
