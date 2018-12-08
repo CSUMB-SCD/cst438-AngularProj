2238a5e661a5f333431b003ed739c20c5ab8084c
Luis Valencia <lvalencia@csumb.edu>
2018-12-07 22:14:29 -0800

    okay never mind

--- server.js
+++ server.js
@@ -8,6 +8,5 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join('./dist/cst438-AngularProj/index.html'));
 });
 
-res.sendFile('index.html', { root: __dirname });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
