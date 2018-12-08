3fe7c15fbca0745db06d5282f83783d5ca82405e
LuisV <lvalencia@csumb.edu>
2018-12-07 20:33:39 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 const express = require('express');
 
 const path = require('path');
+const app = express();
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/index.html'));
   });
 
-const app = express();
 
 
 
