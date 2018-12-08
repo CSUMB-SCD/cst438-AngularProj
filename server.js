5f232aa1e44417d83fe994e9c725f7b2e109a6b9
Luis Valencia <lvalencia@csumb.edu>
2018-12-07 20:59:29 -0800

    I'm getting so tired of this fcking thing

--- server.js
+++ server.js
@@ -3,16 +3,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.static(__dirname + '/dist/cst438-AngularProj'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/cst438-AngularProj/Index.html'));
+  res.sendFile(path.join(__dirname+'/dist/cst438-AngularProj/index.html'));
 });
 
 
 
 // Serve only the static files form the dist directory
 
-app.use(express.static(__dirname + '/dist/cst438-AngularProj'));
 
 
 
